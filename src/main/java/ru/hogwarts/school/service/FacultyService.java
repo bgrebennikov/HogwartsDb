c6d47ce13035c7f7799382f228fb012b6bd452fb
boris <bgrebennikovv@gmail.com>
2026-04-19 13:08:31 +0400

    add FacultyService

---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,40 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class FacultyService {
+    private final HashMap<Long, Faculty> facultyHashMap = new HashMap<>();
+
+    private final AtomicLong idCounter = new AtomicLong();
+
+    public Faculty createFaculty(Faculty faculty) {
+        long id = idCounter.incrementAndGet();
+        faculty.setId(id);
+        facultyHashMap.put(id, faculty);
+        return faculty;
+    }
+
+    public Faculty findFacultyById(Long id) {
+        return facultyHashMap.get(id);
+    }
+
+    public Faculty updateFaculty(Faculty faculty) {
+        if (!facultyHashMap.containsKey(faculty.getId())) {
+            return null;
+        }
+        return facultyHashMap.replace(faculty.getId(), faculty) != null ? faculty : null;
+    }
+
+    public Faculty deleteFacultyById(Long id) {
+        return facultyHashMap.remove(id);
+    }
+
+    public Collection<Faculty> findAllFaculties() {
+        return Collections.unmodifiableCollection(facultyHashMap.values());
+    }
+}
