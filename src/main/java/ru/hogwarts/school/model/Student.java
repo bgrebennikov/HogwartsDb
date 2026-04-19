2e46aa4baca2c5ceed7e1530adb1411307ac2d3e
boris <bgrebennikovv@gmail.com>
2026-04-19 12:45:43 +0400

    add Student model

---
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -0,0 +1,27 @@
+package ru.hogwarts.school.model;
+
+public class Student {
+
+    Long id;
+    String name;
+    int age;
+
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
