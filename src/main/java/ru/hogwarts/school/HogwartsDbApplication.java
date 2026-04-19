d8b7f93ef94df816922cea58d618e0cf4cfb6434
boris <bgrebennikovv@gmail.com>
2026-04-19 12:43:42 +0400

    initial commit

---
+++ src/main/java/ru/hogwarts/school/HogwartsDbApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsDbApplication.class, args);
+	}
+
+}
