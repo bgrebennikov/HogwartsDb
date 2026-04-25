b5fb18345ceba581ed2e26f5fdcd036f63b78330
boris <bgrebennikovv@gmail.com>
2026-04-25 23:47:25 +0400

    add CRUD operations to FacultyController

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -1,8 +1,10 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.school.Faculty;
 import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/faculty")
@@ -14,6 +16,32 @@ public class FacultyController {
         this.facultyService = facultyService;
     }
 
+    @GetMapping
+    public Collection<Faculty> getFaculties() {
+        return facultyService.findAllFaculties();
+    }
+
+    @GetMapping("/{facultyId}")
+    public Faculty getFacultyById(
+            @PathVariable Long facultyId
+    ) {
+        return facultyService.findFacultyById(facultyId);
+    }
+
+    @PostMapping
+    public Faculty createFaculty(@RequestBody Faculty faculty) {
+        return facultyService.createFaculty(faculty);
+    }
+
+    @PatchMapping
+    public Faculty updateFaculty(@RequestBody Faculty faculty) {
+        return facultyService.updateFaculty(faculty);
+    }
+
+    @DeleteMapping("/{facultyId}")
+    public Faculty deleteFaculty(@PathVariable Long facultyId) {
+        return facultyService.deleteFacultyById(facultyId);
+    }
 
 
 }
