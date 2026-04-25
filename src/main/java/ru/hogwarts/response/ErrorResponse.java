ea013d3ff61a6d532a52965a510b0809da9789ec
boris <bgrebennikovv@gmail.com>
2026-04-25 23:38:11 +0400

    add ErrorResponse

---
+++ src/main/java/ru/hogwarts/response/ErrorResponse.java
@@ -0,0 +1,19 @@
+package ru.hogwarts.response;
+
+public class ErrorResponse {
+    String message;
+    int code;
+
+    public ErrorResponse(String message, int code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
