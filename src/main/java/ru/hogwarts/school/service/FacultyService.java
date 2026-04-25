f6bc6f7dd25dae6a86e1cd1e6ccb9446ecca307a
boris <bgrebennikovv@gmail.com>
2026-04-25 23:39:14 +0400

    move FacultyService to school package

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,7 +1,7 @@
 package ru.hogwarts.school.service;
 
 import org.springframework.stereotype.Service;
-import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.school.Faculty;
 
 import java.util.Collection;
 import java.util.Collections;
