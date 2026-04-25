b5a7a2ff4eaa8431499268263398334bec4f497e
boris <bgrebennikovv@gmail.com>
2026-04-25 23:39:26 +0400

    Add GlobalExceptionHandler

---
+++ src/main/java/ru/hogwarts/school/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package ru.hogwarts.school.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.hogwarts.response.ErrorResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleException(Exception ex) {
+        return ResponseEntity.badRequest().body(
+                new ErrorResponse(
+                        ex.getMessage(),
+                        HttpStatus.BAD_REQUEST.value()
+                )
+        );
+    }
+}
