3ff41fcea7a33338c3c9e94e86821107d0466a4d
boris <bgrebennikovv@gmail.com>
2026-04-29 22:46:47 +0400

    using dto for request body

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.dto.request.FacultyRequest;
+import ru.hogwarts.school.model.dto.request.FacultyUpdateRequest;
 import ru.hogwarts.school.model.school.Faculty;
 import ru.hogwarts.school.service.FacultyService;
 
@@ -29,7 +32,7 @@ public class FacultyController {
             description = "Возвращает все факультеты, отфильтрованные по конкретному цвету."
     )
     public Collection<Faculty> getFaculties(
-            @RequestParam @Parameter(description = "Цвет факультета для поиска", example = "Gryffindor Red") String color
+            @RequestParam(required = false) @Parameter(description = "Цвет факультета для поиска") String color
     ) {
         return facultyService.findAllFaculties(color);
     }
@@ -52,17 +55,19 @@ public class FacultyController {
             summary = "Добавить новый факультет",
             description = "Создает новый факультет в системе. ID генерируется автоматически."
     )
-    public Faculty createFaculty(@RequestBody Faculty faculty) {
+    public Faculty createFaculty(@RequestBody FacultyRequest faculty) {
         return facultyService.createFaculty(faculty);
     }
 
-    @PatchMapping
+    @PatchMapping("/{id}")
     @Operation(
             summary = "Обновить данные факультета",
-            description = "Изменяет параметры существующего факультета. В теле запроса обязателен корректный ID."
+            description = "Изменяет параметры существующего факультета."
     )
-    public Faculty updateFaculty(@RequestBody Faculty faculty) {
-        return facultyService.updateFaculty(faculty);
+    public ResponseEntity<Faculty> updateFaculty(
+            @PathVariable Long id,
+            @Valid @RequestBody FacultyUpdateRequest faculty) {
+        return ResponseEntity.ok(facultyService.updateFaculty(id, faculty));
     }
 
     @DeleteMapping("/{facultyId}")
