d7252177dc97332b312a8d9720fd2538fcd821f0
boris <bgrebennikovv@gmail.com>
2026-04-29 19:05:50 +0400

    add postgresql config

--- src/main/java/ru/hogwarts/school/HogwartsDbApplication.java
+++ src/main/java/ru/hogwarts/school/HogwartsDbApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-		exclude = {DataSourceAutoConfiguration.class}
-)
+@SpringBootApplication()
 public class HogwartsDbApplication {
 
 	public static void main(String[] args) {
