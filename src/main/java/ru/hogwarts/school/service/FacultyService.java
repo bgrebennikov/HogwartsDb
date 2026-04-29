38f9db8a60e26dc92d7b4b0f15f2722e0738f868
boris <bgrebennikovv@gmail.com>
2026-04-29 22:48:02 +0400

    using dto for Faculty entity

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -2,10 +2,13 @@ package ru.hogwarts.school.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.dto.request.FacultyRequest;
+import ru.hogwarts.school.model.dto.request.FacultyUpdateRequest;
 import ru.hogwarts.school.model.school.Faculty;
 import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.Collection;
+import java.util.NoSuchElementException;
 
 @Service
 public class FacultyService {
@@ -17,16 +20,27 @@ public class FacultyService {
         this.facultyRepository = facultyRepository;
     }
 
-    public Faculty createFaculty(Faculty faculty) {
-        return facultyRepository.save(faculty);
+    public Faculty createFaculty(FacultyRequest faculty) {
+        return facultyRepository.save(asEntity(faculty));
     }
 
     public Faculty findFacultyById(Long id) {
         return facultyRepository.findById(id).orElseThrow();
     }
 
-    public Faculty updateFaculty(Faculty faculty) {
-        return facultyRepository.save(faculty);
+    public Faculty updateFaculty(Long id, FacultyUpdateRequest request) {
+        return facultyRepository.findById(id)
+                .map(f -> {
+                    f.setName(request.name());
+                    f.setColor(request.color());
+                    return facultyRepository.save(f);
+                })
+                .orElseThrow(
+                        () -> new NoSuchElementException(
+                                "Not found faculty with ID: %s"
+                                        .formatted(id)
+                        )
+                );
     }
 
     public void deleteFacultyById(Long id) {
@@ -38,5 +52,12 @@ public class FacultyService {
             return facultyRepository.findAll();
         }
         return facultyRepository.findAllByColorIgnoreCase(color);
+    }
+
+    private Faculty asEntity(FacultyRequest dto) {
+        return new Faculty(
+                dto.name(),
+                dto.color()
+        );
     }
 }
