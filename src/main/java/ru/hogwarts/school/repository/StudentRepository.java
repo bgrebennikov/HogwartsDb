fcd42a5817aa9581810a7edf194df59519cd97d7
boris <bgrebennikovv@gmail.com>
2026-04-29 19:23:49 +0400

    add StudentRepository

---
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.hogwarts.school.model.school.Student;
+
+import java.util.Collection;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Collection<Student> findAllByAgeBetween(int min, int max);
+}
