727d3621864a94f52ac47cc16a6e6f1a02ac7782
boris <bgrebennikovv@gmail.com>
2026-04-29 19:33:33 +0400

    add filter by color on findAll

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.school.Faculty;
 
+import java.util.Collection;
+
 @Repository
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+
+    Collection<Faculty> findAllByColorIgnoreCase(String color);
 
 }
