db10a20faabc720195a67235091d22043bb420bb
boris <bgrebennikovv@gmail.com>
2026-05-05 20:46:10 +0400

    add scripts.sql

---
+++ scripts.sql
@@ -0,0 +1,9 @@
+select * from student where age between 20 and 30;
+
+select name from student;
+
+select * from student where  name ilike '%a%';
+
+select * from student where age < id;
+
+select * from student order by age
