eabc4db4fbaad541bf40019b0e8c7c38be56ab3d
boris <bgrebennikovv@gmail.com>
2026-05-06 23:43:41 +0400

    add AvatarController

---
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -0,0 +1,15 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/avatar")
+public class AvatarController {
+
+
+    public AvatarController() {
+
+    }
+
+}
