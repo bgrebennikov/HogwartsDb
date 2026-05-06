a251fd18f216561a904ce862c66895a66265f5f6
boris <bgrebennikovv@gmail.com>
2026-05-06 23:31:32 +0400

    add Avatar entity

---
+++ src/main/java/ru/hogwarts/school/model/school/Avatar.java
@@ -0,0 +1,80 @@
+package ru.hogwarts.school.model.school;
+
+import jakarta.persistence.*;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity(name = "avatar")
+public class Avatar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+
+    @Lob
+    private byte[] data;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    public Avatar() {
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Avatar avatar = (Avatar) o;
+        return fileSize == avatar.fileSize && Objects.equals(id, avatar.id) && Objects.equals(filePath, avatar.filePath) && Objects.equals(mediaType, avatar.mediaType) && Objects.deepEquals(data, avatar.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, filePath, fileSize, mediaType, Arrays.hashCode(data));
+    }
+}
