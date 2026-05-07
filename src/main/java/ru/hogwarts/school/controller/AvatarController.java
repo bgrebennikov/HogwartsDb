e4ce713f86f3d98981f7f6dab4a196f8dc85733e
boris <bgrebennikovv@gmail.com>
2026-05-07 17:20:37 +0400

    add AvatarService field

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -2,14 +2,18 @@ package ru.hogwarts.school.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.hogwarts.school.service.AvatarService;
 
 @RestController
 @RequestMapping("/avatar")
 public class AvatarController {
 
+    private final AvatarService avatarService;
 
-    public AvatarController() {
-
+    public AvatarController(AvatarService avatarService) {
+        this.avatarService = avatarService;
     }
+
+
 
 }
