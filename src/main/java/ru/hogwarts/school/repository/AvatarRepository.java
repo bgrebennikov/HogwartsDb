9f44db3fe6fa8ceb2aa48d2f8ea0e0cc663223e3
boris <bgrebennikovv@gmail.com>
2026-05-12 14:19:01 +0400

    add function findByStudentId

--- src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.school.Avatar;
 
+import java.util.Optional;
+
 @Repository
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
 
 
+    public Optional<Avatar> findByStudentId(Long studentId);
 
 
 }
