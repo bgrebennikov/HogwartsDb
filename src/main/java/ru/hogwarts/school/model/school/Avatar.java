7449e0051908da99a43a920d9b3104c2d06aefa2
boris <bgrebennikovv@gmail.com>
2026-05-18 22:04:44 +0400

    add setter for id

--- src/main/java/ru/hogwarts/school/model/school/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/school/Avatar.java
@@ -30,6 +30,10 @@ public class Avatar {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getFilePath() {
         return filePath;
     }
